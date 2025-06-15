8fe06818dc75686109cdf881e2ce907f3ba25184
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-16 10:39:06 +1200

    feat: create an actual logger class and implement debug log to only log if the plugin's debugMode is true

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,30 @@
-// Taken from https://github.com/reorx/obsidian-paste-image-rename
-export function debugLog(...args: any[]) {
-	console.log(new Date().toISOString().slice(11, 23), ...args);
+import WikipediaPastePlugin from "./main";
+
+enum LogLevel {
+	ERROR,
+	WARN,
+	INFO,
+	DEBUG,
+}
+
+type LogLevelStrings = keyof typeof LogLevel;
+
+export class Logger {
+	plugin: WikipediaPastePlugin;
+
+	constructor(plugin: WikipediaPastePlugin) {
+		this.plugin = plugin;
+	}
+
+	protected formatLog(level: LogLevelStrings, ...args: any[]): string {
+		return `${new Date().toISOString()} ${level} ${args.join(" ")}`;
+	}
+
+	debugLog(...args: any[]): void {
+		if (!this.plugin.settings.debugMode) {
+			return;
+		}
+		const logMessage = this.formatLog("DEBUG", args);
+		console.log(logMessage);
+	}
 }
