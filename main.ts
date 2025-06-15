fca3464df08badadf6d18c1de10f0f40dd9350da
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-15 14:32:17 +1200

    feat: add initial block latex functionality

--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ import { MarkdownView, Plugin } from "obsidian";
 const WIKIPEDIA_LINK_SELECTOR = 'a[href*=".wikipedia.org/wiki/"]';
 const WIKIPEDIA_CITATION_SELECTOR = 'sup.reference[id^="cite_ref"]';
 const WIKIPEDIA_INLINE_LATEX_SELECTOR = "body > span.mwe-math-element-inline";
+const WIKIPEDIA_SPAN_BLOCK_LATEX_MATH_SELECTOR =
+	"body span.mwe-math-element-block";
+const WIKIPEDIA_DL_BLOCK_LATEX_MATH_SELECTOR =
+	"body > dl span.mwe-math-element-inline math";
 
 const WIKIPEDIA_LATEX_EXTRACT = /\{\\displaystyle (.*) ?\}/;
 
@@ -54,6 +58,7 @@ export default class WikipediaLatexPastePlugin extends Plugin {
 				replaceWikipediaLinks(toPasteHTML);
 				removeCitations(toPasteHTML);
 				replaceInlineLatex(toPasteHTML);
+				replaceBlockLatex(toPasteHTML);
 
 				console.log("after HTML:\n", toPasteHTML);
 
@@ -74,6 +79,7 @@ function doesDocumentContainWikipediaElements(document: Document): boolean {
 		doesDocumentHaveWikipediaLinks,
 		doesDocumentHaveCitations,
 		doesDocumentHaveInlineLatex,
+		doesDocumentHaveBlockLatex,
 	];
 
 	for (let i = 0; i < wikipediaElementCheckers.length; i++) {
@@ -150,5 +156,49 @@ function replaceInlineLatex(document: Document): void {
 
 		const latex = extractLatexFromAltText(rawLatex);
 		inlineLatexContainingSpan.replaceWith(`$${latex}$`);
+	}
+}
+
+function doesDocumentHaveBlockLatex(document: Document): boolean {
+	const numDlBlockLatex = document.querySelectorAll(
+		WIKIPEDIA_DL_BLOCK_LATEX_MATH_SELECTOR,
+	).length;
+
+	const numSpanBlockLatex = document.querySelectorAll(
+		WIKIPEDIA_SPAN_BLOCK_LATEX_MATH_SELECTOR,
+	).length;
+
+	return numDlBlockLatex + numSpanBlockLatex > 0;
+}
+
+function replaceBlockLatex(document: Document): void {
+	for (const dlElement of document.querySelectorAll("dl")) {
+		for (const blockLatexMathElement of dlElement.querySelectorAll(
+			WIKIPEDIA_DL_BLOCK_LATEX_MATH_SELECTOR,
+		)) {
+			const rawLatex = blockLatexMathElement.getAttribute("alttext");
+
+			if (rawLatex == null) {
+				continue;
+			}
+
+			const latex = extractLatexFromAltText(rawLatex);
+			dlElement.replaceWith(`$$${latex}$$\n`);
+		}
+	}
+
+	for (const blockLatexContainingSpan of document.querySelectorAll(
+		WIKIPEDIA_SPAN_BLOCK_LATEX_MATH_SELECTOR,
+	)) {
+		const rawLatex = blockLatexContainingSpan
+			.getElementsByTagName("math")[0]
+			.getAttribute("alttext");
+
+		if (rawLatex == null) {
+			continue;
+		}
+
+		const latex = extractLatexFromAltText(rawLatex);
+		blockLatexContainingSpan.replaceWith(`$$${latex}$$`);
 	}
 }
