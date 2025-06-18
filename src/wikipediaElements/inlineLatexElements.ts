3a981f408ff78676f2a3b4bff40d5bc3318df93a
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-18 21:49:59 +1200

    feat: start parsing latex that are in texhtml span tags

--- src/wikipediaElements/inlineLatexElements.ts
+++ src/wikipediaElements/inlineLatexElements.ts
@@ -1,16 +1,18 @@
-import { extractLatexFromAltText } from "src/utils";
+import { extractLatexFromAltText, parseUnicodeToLatex } from "src/utils";
 
-const WIKIPEDIA_INLINE_LATEX_SELECTOR = "body span.mwe-math-element-inline";
+const MWL_MATH_ELEMENT_INLINE_SELECTOR = "body span.mwe-math-element-inline";
+const TEXHTML_SPAM_SELECTOR = "body span.texhtml";
 
 export function doesDocumentHaveInlineLatex(document: Document): boolean {
-	return (
-		document.querySelectorAll(WIKIPEDIA_INLINE_LATEX_SELECTOR).length > 0
-	);
+	const inlineLatexCount =
+		document.querySelectorAll(MWL_MATH_ELEMENT_INLINE_SELECTOR).length +
+		document.querySelectorAll(TEXHTML_SPAM_SELECTOR).length;
+	return inlineLatexCount > 0;
 }
 
 export function replaceInlineLatex(document: Document): void {
 	for (const inlineLatexContainingSpan of document.querySelectorAll(
-		WIKIPEDIA_INLINE_LATEX_SELECTOR,
+		MWL_MATH_ELEMENT_INLINE_SELECTOR,
 	)) {
 		const rawLatex = inlineLatexContainingSpan
 			.getElementsByTagName("math")[0]
@@ -21,6 +23,24 @@ export function replaceInlineLatex(document: Document): void {
 		}
 
 		const latex = extractLatexFromAltText(rawLatex);
+		inlineLatexContainingSpan.replaceWith(`$${latex}$`);
+	}
+
+	// TODO: Sup and sub tags
+	for (const inlineLatexContainingSpan of document.querySelectorAll(
+		TEXHTML_SPAM_SELECTOR,
+	)) {
+		const text = inlineLatexContainingSpan.textContent;
+		if (text == null) {
+			continue;
+		}
+
+		let latex = "";
+
+		for (const char of text) {
+			latex += parseUnicodeToLatex(char);
+		}
+
 		inlineLatexContainingSpan.replaceWith(`$${latex}$`);
 	}
 }
