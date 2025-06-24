389161e5321ba9c5ef7c9b7a2df87c9513339a21
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-24 16:09:21 +1200

    fix: add checks to see if a link is a wikipedia page

--- src/wikipediaElements/linkElements.ts
+++ src/wikipediaElements/linkElements.ts
@@ -1,11 +1,37 @@
-const WIKIPEDIA_LINK_SELECTOR = 'body > a[href*=".wikipedia.org/wiki/"]';
+const WIKIPEDIA_LINK_SELECTOR = 'body a[href*=".wikipedia.org/wiki/"]';
+
+function isAnchorTagWikipediaLink(link: Element): boolean {
+	const linkHref = link.getAttribute("href");
+
+	if (
+		linkHref === "https://en.wikipedia.org/wiki/Wikipedia:Citation_needed"
+	) {
+		return false;
+	}
+
+	if (!linkHref?.includes("#")) {
+		return true;
+	}
+
+	const splitHref = linkHref.split("#");
+	const header = splitHref[splitHref.length - 1];
+	return !header.startsWith("cite_note");
+}
 
 export function doesDocumentHaveWikipediaLinks(document: Document): boolean {
-	return document.querySelectorAll(WIKIPEDIA_LINK_SELECTOR).length > 0;
+	return (
+		[...document.querySelectorAll(WIKIPEDIA_LINK_SELECTOR)].filter(
+			isAnchorTagWikipediaLink,
+		).length > 0
+	);
 }
 
 export function replaceWikipediaLinks(document: Document): void {
 	document.querySelectorAll(WIKIPEDIA_LINK_SELECTOR).forEach((link) => {
+		if (isAnchorTagWikipediaLink(link)) {
+			return;
+		}
+
 		const linkTitle = link.getAttribute("title");
 		let linkText = link.textContent;
 
