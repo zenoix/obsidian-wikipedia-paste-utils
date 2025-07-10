b1117fa2fc2f6cbcb5f9580039c0823338168f7a
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-10 16:07:33 +1200

    test: initial test template for doesDocumentHaveBoldText

---
+++ __tests__/wikipediaElements/boldElements.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+import { doesDocumentHaveBoldText } from "src/wikipediaElements/boldElements";
+
+describe("doesDocumentHaveBoldText", () => {
+    describe("no bold text", () => {});
+    describe("has bold text", () => {});
+});
