16edd2e0eee5b46bc30de3a09e0b05994e5fba70
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-15 10:26:21 +1200

    feat: add translate bold elements

--- src/wikipediaElements/boldElements.ts
+++ src/wikipediaElements/boldElements.ts
@@ -3,3 +3,11 @@ const BOLD_SELECTOR = "b";
 export function doesDocumentHaveBoldText(document: Document): boolean {
     return document.querySelectorAll(BOLD_SELECTOR).length > 0;
 }
+
+export function translateBoldElements(document: Document): void {
+    document.querySelectorAll(BOLD_SELECTOR).forEach((boldText) => {
+        let output = boldText.textContent || "";
+
+        boldText.replaceWith(`**${output.trim()}**`);
+    });
+}
