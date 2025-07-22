57cb0b3a410619d819e663791d5a37d725fa14e5
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-23 11:31:20 +1200

    style: format

--- src/wikipediaElements/inlineLatexElements.ts
+++ src/wikipediaElements/inlineLatexElements.ts
@@ -4,54 +4,54 @@ const MWL_MATH_ELEMENT_INLINE_SELECTOR = "body span.mwe-math-element-inline";
 const TEXHTML_SPAM_SELECTOR = "body span.texhtml";
 
 export function doesDocumentHaveInlineLatex(document: Document): boolean {
-	const inlineLatexCount =
-		document.querySelectorAll(MWL_MATH_ELEMENT_INLINE_SELECTOR).length +
-		document.querySelectorAll(TEXHTML_SPAM_SELECTOR).length;
-	return inlineLatexCount > 0;
+    const inlineLatexCount =
+        document.querySelectorAll(MWL_MATH_ELEMENT_INLINE_SELECTOR).length +
+        document.querySelectorAll(TEXHTML_SPAM_SELECTOR).length;
+    return inlineLatexCount > 0;
 }
 
 function translateSupElements(htmlString: string): string {
-	return htmlString.replace(/<sup>/g, "^{").replace(/<\/sup>/g, "}");
+    return htmlString.replace(/<sup>/g, "^{").replace(/<\/sup>/g, "}");
 }
 
 function translateSubElements(htmlString: string): string {
-	return htmlString.replace(/<sub>/g, "_{").replace(/<\/sub>/g, "}");
+    return htmlString.replace(/<sub>/g, "_{").replace(/<\/sub>/g, "}");
 }
 
 export function replaceInlineLatex(document: Document): void {
-	for (const inlineLatexContainingSpan of document.querySelectorAll(
-		MWL_MATH_ELEMENT_INLINE_SELECTOR,
-	)) {
-		const rawLatex = inlineLatexContainingSpan
-			.getElementsByTagName("math")[0]
-			.getAttribute("alttext");
+    for (const inlineLatexContainingSpan of document.querySelectorAll(
+        MWL_MATH_ELEMENT_INLINE_SELECTOR,
+    )) {
+        const rawLatex = inlineLatexContainingSpan
+            .getElementsByTagName("math")[0]
+            .getAttribute("alttext");
 
-		if (rawLatex == null) {
-			continue;
-		}
+        if (rawLatex == null) {
+            continue;
+        }
 
-		const latex = extractLatexFromAltText(rawLatex).trim();
-		inlineLatexContainingSpan.replaceWith(`$${latex}$`);
-	}
+        const latex = extractLatexFromAltText(rawLatex).trim();
+        inlineLatexContainingSpan.replaceWith(`$${latex}$`);
+    }
 
-	for (const inlineLatexContainingSpan of document.querySelectorAll(
-		TEXHTML_SPAM_SELECTOR,
-	)) {
-		const text = inlineLatexContainingSpan.innerHTML;
-		console.log(text);
-		if (text == null) {
-			continue;
-		}
+    for (const inlineLatexContainingSpan of document.querySelectorAll(
+        TEXHTML_SPAM_SELECTOR,
+    )) {
+        const text = inlineLatexContainingSpan.innerHTML;
+        console.log(text);
+        if (text == null) {
+            continue;
+        }
 
-		let latex = "";
+        let latex = "";
 
-		for (const char of text) {
-			latex += parseUnicodeToLatex(char);
-		}
+        for (const char of text) {
+            latex += parseUnicodeToLatex(char);
+        }
 
-		latex = translateSupElements(latex);
-		latex = translateSubElements(latex);
+        latex = translateSupElements(latex);
+        latex = translateSubElements(latex);
 
-		inlineLatexContainingSpan.replaceWith(`$${latex}$`);
-	}
+        inlineLatexContainingSpan.replaceWith(`$${latex}$`);
+    }
 }
